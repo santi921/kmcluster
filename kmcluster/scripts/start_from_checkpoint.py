4f678873bc70a80f2998e03c38326fe5216a273e
Santiago Vargas <santiagovargas921@gmail.com>
2023-04-24 13:33:58 -0700

    update save file

--- kmcluster/scripts/start_from_checkpoint.py
+++ kmcluster/scripts/start_from_checkpoint.py
@@ -1,7 +1,15 @@
-from kmcluster.core.kmc_minimum import kmc
-from kmcluster.core.transition_conditions import rfkmc
-from kmcluster.core.intialize import random_init, boltz
+from kmcluster.core.kmc import kmc, load_kmc_from_matrix
+from kmcluster.core.transition_conditions import rfkmc, rkmc
+from kmcluster.core.intialize import random_init, boltz, selected
 from kmcluster.core.data import sparse_to_mat
+
+from kmcluster.core.viz import (
+    plot_top_n_states,
+    plot_states,
+    graph_trajectories_static,
+    communities_static,
+    compute_state_counts,
+)
 
 
 def get_merge_data():
@@ -105,166 +113,71 @@ def get_merge_data():
     return Pt_H1_all, H1_E
 
 
-def get_merge_data_new():
-    Pt4H3_relE = [
-        0.0,
-        0.05265172000000007,
-        0.08150556000000009,
-        0.08234174999999766,
-        0.10898511999999982,
-        0.13161836000000093,
-        0.15918047999999985,
-        0.16211587000000094,
-        0.17019705999999957,
-        0.19589320000000043,
-        0.2124428199999997,
-        0.21772393999999906,
-        0.23981415000000084,
-        0.2480630899999987,
-        0.24823677999999916,
-        0.2504169300000001,
-        0.26229379999999836,
-        0.2676832299999994,
-        0.2842788299999981,
-        0.29314600000000013,
-        0.2996583599999987,
-        0.3080355399999988,
-        0.3220917700000001,
-        0.3320207499999981,
-        0.3357182899999991,
-        0.34816155999999765,
-    ]
-    Pt4H3_links = [
-        [0, 3, 0.39],
-        [1, 5, 0.27],
-        [1, 16, 0.39],
-        [2, 16, 0.55],
-        [2, 19, 0.55],
-        [3, 19, 0.23],
-        [4, 18, 0.22],
-        [6, 14, 0.17],
-        [6, 8, 0.17],
-        [7, 14, 0.14],
-        [7, 24, 0.37],
-        [12, 16, 0.23],
-        [16, 19, 0.35],
-        [20, 23, 0.45],
-        [0, 10, 0.27],
-        [0, 18, 0.74],
-        [0, 22, 0.43],
-        [1, 9, 0.17],
-        [2, 13, 0.36],
-        [3, 10, 0.34],
-        [5, 14, 0.17],
-        [5, 15, 0.31],
-        [5, 25, 0.67],
-        [12, 22, 0.61],
-        [11, 18, 0.27],
-        [15, 17, 0.16],
-        [7, 17, 0.36],
-        [20, 21, 0.26],
-    ]
-    # calculating the reverse barriers
-    Pt4H3_rev = []
-    for i in range(0, len(Pt4H3_links)):
-        Pt4H3_rev.append(
-            [
-                Pt4H3_links[i][1],
-                Pt4H3_links[i][0],
-                round(
-                    (Pt4H3_links[i][2] + Pt4H3_relE[Pt4H3_links[i][0]])
-                    - Pt4H3_relE[Pt4H3_links[i][1]],
-                    2,
-                ),
-            ]
-        )
-    # all barriers
-    Pt4H3_all = Pt4H3_links + Pt4H3_rev
-    return Pt4H3_all, Pt4H3_relE
-
-
 # main function
 if __name__ == "__main__":
     # mess with this data to input yours, ill eventually make a read file
-    T_kelvin = 100
+    T_kelvin = 150
     Pt_H2_all, Pt4H2_relE = get_merge_data()
-
     energies_mat = sparse_to_mat(Pt_H2_all)
-    # print(energies_mat)
-    temp_boltz = T_kelvin * 8.617 * 10**-5
-    # k_b_ev = 8.614 * 10**-5
 
-    energies_nonzero = energies_mat[energies_mat != 0]
+    energies_nonzero = [x for x in Pt4H2_relE if x != 0]
     print("smallest barrier: ", min(energies_nonzero))
+    temp_boltz = T_kelvin * 8.617 * 10 ** (-5)
+    k_b_ev = 8.614 * 10**-5
 
-    rfkmc_obj = rfkmc(k_b_t=temp_boltz, energy_mat=energies_mat)
+    rfkmc_obj = rfkmc(k_b_t=temp_boltz)    
     time_stop = 0.0001
-
     # initialize kmc object
-    kmc_boltz = kmc(
-        time_stop=0.0001,
-        energies=energies_mat,
-        draw_crit=rfkmc_obj,
-        initialization=None,
-        checkpoint=False,
-        # checkpoint_dir="./checkpoints_restart/",
-        # state_dict_file="./checkpoints/Pt4H2_g_200__trajectories_3_ckpt.pkl",
-        batch_size=1000,
-        sample_frequency=1000,
-    )
+
+
+    file = "./checkpoints/saved_data_trajectories_10_final_ckpt_start_0_end_1.00000e-04_step_1.00000e-06.npy"
+    #file = "./checkpoints/trajectories_9_ckpt_start_0_end_9.00000e-05_step_9.00000e-07.npy"
+    kmc_boltz = load_kmc_from_matrix(
+        file, 
+        energies_mat, 
+        rfkmc_obj,
+        time_stop,
+        )
 
     print("running kmc")
-    kmc_boltz.run(n_steps=-1)
-    print("kmc done")
 
-    n_show = -1
-
-    # plot line - select
-    kmc_boltz.plot_select_states(
-        states_to_plot=[0, 1, 2, 5, 6],
+    n_show = 5
+    print("mpl plot - top states")
+    kmc_boltz.plot_top_n_states(
+        n_show=n_show,
+        #total_states=len(Pt4H2_relE),
+        resolution=0.000001,
         max_time=0.0001,
         title="State Distribution, {}K".format(T_kelvin),
         xlabel="Time (s)",
         ylabel="Population Proportion",
         save=True,
-        save_name="./plots/Pt4H2_g_{}_top{}.png".format(T_kelvin, n_show),
+	    save_name="Pt4H2_g_150K_top{}.png".format(n_show)
     )
 
-    # stacked - top n
-
+    """
+    print("plotly plot - top states")
+    
     kmc_boltz.plot_top_n_states_stacked(
-        n_show=-1,
-        max_time=0.0001,
-        title="State Distribution, {}K".format(T_kelvin),
+    	n_show = 5,
+    	resolution=0.000001, 
+    	max_time=None, 
+    	title="State Distribution, {}K".format(T_kelvin),
         xlabel="Time (s)",
-        ylabel="Population Proportion",
-        save=True,
+        ylabel="Population Proportion", 
+    	save=True, 
         show=True,
-        save_name="./plots/Pt4H2_stacked_{}_top{}.png".format(T_kelvin, n_show),
-    )
-
-    # stacked - select
-
+    	save_name="Pt4H2_g_stacked_150K.png")
+    print("plotly plot - select states")
     kmc_boltz.plot_select_states_stacked(
         states_to_plot=[0, 1, 2, 5, 6],
-        max_time=0.0001,
-        title="State Distribution, {}K".format(T_kelvin),
+        resolution=0.000001, 
+        max_time=None, 
+    	title="State Distribution, {}K".format(T_kelvin),
         xlabel="Time (s)",
-        ylabel="Population Proportion",
-        save=True,
+        ylabel="Population Proportion",     
+     	save=True, 
         show=True,
-        save_name="./plots/Pt4H2_g_stacked_select_{}.png".format(T_kelvin),
+        save_name="Pt4H2_g_stacked_select_150K.png"
     )
-
-    # plot line - top n
-
-    kmc_boltz.plot_top_n_states(
-        n_show=-1,
-        max_time=0.0001,
-        title="State Distribution, {}K".format(T_kelvin),
-        xlabel="Time (s)",
-        ylabel="Population Proportion",
-        save=True,
-        show=True,
-        save_name="./plots/Pt4H2_stacked_{}_top{}.png".format(T_kelvin, n_show),
-    )
+    """
